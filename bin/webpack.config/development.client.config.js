15de6e75dd266795841339b5a6761f2bb1de43fb
shengyao <MY_NAME@example.com>
2020-06-21 22:36:41 +0800

    update

---
+++ bin/webpack.config/development.client.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const path = require('path')
+const baseConfig = require('./base.config')
+const CreatHtmlPlugin = require('../plugins/creat-html-plugin')
+const { config } = require('../utils/common')
+const __baseConfig = baseConfig(config)
+const { port = 9000, host = '0.0.0.0' } = config
+
+const CWD = process.cwd()
+const buildPath =  path.resolve(CWD, config.build)
+
+const jsName  = 'js/[name]@dev.js'
+const cssName = 'css/[name]@dev.css'
+
+const plugins = () => {
+  let __plugins = [
+      ...CreatHtmlPlugin('development',__baseConfig)
+    ]
+    return __plugins
+}
+
+module.exports = () => {
+  return merge(__baseConfig, {
+    output: {
+      path: buildPath,
+      publicPath: config.static[process.env.MODE],
+      chunkFilename: 'js/[name]-[chunkhash:8].js',
+      filename: jsName
+    },
+    mode: 'development',
+    devtool: 'none',
+    performance: {
+      hints: false,
+    },
+    plugins: plugins(),
+    devServer: {
+      contentBase: buildPath,
+      compress: true,
+      host: host,
+      port: port,
+      hot: true,
+      inline: true,
+      historyApiFallback: true
+
+    }
+  })
+}
---
+++ bin/webpack.config/production.client.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CreatVersionPlugin = require('../plugins/creat-version-plugin')
+const CreatHtmlPlugin = require('../plugins/creat-html-plugin')
+
+const baseConfig = require('./base.config')
+
+const { config } = require('../utils/common')
+
+const path = require('path')
+const CWD = process.cwd()
+
+const buildPath =  path.resolve(CWD, config.build)
+
+const jsName  = 'js/[name]@[chunkhash].js'
+const cssName = 'css/[name]@[chunkhash].css'
+
+let __baseConfig = baseConfig(config)
+__baseConfig.module.rules.map(item => {
+  if (/css|sass|less/.test(item.use)) {
+    item.use.shift()
+    item.use = {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            hmr: true
+        }
+    }
+  }
+})
+
+const plugins = [
+  new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      // filename: 'f_[name].css',
+      filename: cssName,
+      chunkFilename: cssName,
+      ignoreOrder: true // Enable to remove warnings about conflicting order
+  }),
+  new CreatVersionPlugin(),
+  ...CreatHtmlPlugin('production',__baseConfig)
+]
+
+
+
+
+module.exports = () => {
+  return merge(__baseConfig, {
+    output: {
+      path: buildPath,
+      publicPath: config.static[process.env.MODE],
+      chunkFilename: 'js/[name]-[chunkhash:8].js',
+      filename: jsName
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    plugins,
+    performance: {
+      hints: false,
+    }
+  })
+}
