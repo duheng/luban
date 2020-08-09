e5ea914edcc4c96f421052696ff253fcd501ffeb
shengyao <MY_NAME@example.com>
2020-08-09 17:25:23 +0800

    update

--- bin/dev.server/index.js
+++ bin/dev.server/index.js
@@ -1,52 +1,72 @@
-const path = require('path')
-const fs = require('fs')
-const Webpack = require('webpack');
-const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware')
-const __config = require('../webpack.config/development.client.config')();
+const path = require("path");
+const fs = require("fs");
+const Webpack = require("webpack");
+const { devMiddleware, hotMiddleware } = require("koa-webpack-middleware");
+const __config = require("../webpack.config/development.client.config")();
 
-const koa = require('koa');
+const koa = require("koa");
 const app = new koa();
 
 const formatConfig = (config) => {
-  let __config = {...config}
-  let __entry = __config.entry
-  let __entryNew = {}
-  if(typeof(__entry) != 'object') {
-    throw new Error('entry必须object类型\n 例如："entry": {"main":"./src/pages/index.js"}\r\n')
+  let __config = { ...config };
+  let __entry = __config.entry;
+  let __entryNew = {};
+  if (typeof __entry !== "object") {
+    throw new Error(
+      'entry必须object类型\n 例如："entry": {"main":"./src/pages/index.js"}\r\n'
+    );
   }
 
-  for(let i in __entry){
-    const __entryFile= String(fs.readFileSync(path.resolve(__entry[i])))
-    if(!/\bmodule.hot\b/.test(__entryFile)){
-      console.log('\n系统检测到入口文件缺少热更新必须的module.hot，系统已为您自动添加\n')
-      fs.appendFileSync(path.resolve(__entry[i]), ';if (module.hot) {module.hot.accept()};')
+  for (let i in __entry) {
+    try {
+      const __entryFile = String(fs.readFileSync(path.resolve(__entry[i])));
+      if (!/\bmodule.hot\b/.test(__entryFile)) {
+        console.log(
+          "\n系统检测到入口文件缺少热更新必须的module.hot，系统已为您自动添加\n"
+        );
+        fs.appendFileSync(
+          path.resolve(__entry[i]),
+          ";if (module.hot) {module.hot.accept()};"
+        );
+      }
+    } catch (err) {
+      throw `入口文件加载失败，请检查入口文件\r\n${err}`;
     }
-    __entryNew[i] = [__entry[i],'webpack-hot-middleware/client?reload=false&path=/__webpack_hmr&timeout=20000']
+
+    __entryNew[i] = [
+      __entry[i],
+      "webpack-hot-middleware/client?reload=false&path=/__webpack_hmr&timeout=20000",
+    ];
   }
-  __config.entry = __entryNew
-  return __config
-}
+  __config.entry = __entryNew;
+  return __config;
+};
 
-const indexHtml = (url) => { // 页面重定向匹配
-   const entrys = Object.keys(config.entry)
-   let __indexname = ''
-   if(entrys.length == 1) { // 单页面应用只有一个入口，读取第一个入口作为路由入口页面，起着承载路由的作用
-     __indexname = entrys[0]
-   } else { // 多页面需要在系统入口文件中匹配请求过来的页面，没有匹配到则返回404页面
-     const __reqpage = url.split('/').pop().split('.')[0]
-     __indexname = entrys.indexOf(__reqpage) > -1 ? __reqpage : '404'
-   }
-   return `${__indexname}.html`
-}
+const indexHtml = (url) => {
+  // 页面重定向匹配
+  const entrys = Object.keys(config.entry);
+  let __indexname = "";
+  if (entrys.length == 1) {
+    // 单页面应用只有一个入口，读取第一个入口作为路由入口页面，起着承载路由的作用
+    __indexname = entrys[0];
+  } else {
+    // 多页面需要在系统入口文件中匹配请求过来的页面，没有匹配到则返回404页面
+    const __reqpage = url
+      .split("/")
+      .pop()
+      .split(".")[0];
+    __indexname = entrys.indexOf(__reqpage) > -1 ? __reqpage : "404";
+  }
+  return `${__indexname}.html`;
+};
 
-const config = formatConfig(__config)
+const config = formatConfig(__config);
 const compile = Webpack(config);
-
 
 app.use(
   devMiddleware(compile, {
     noInfo: false,
-    hot:false,
+    hot: false,
     publicPath: config.output.publicPath,
     stats: {
       colors: true,
@@ -54,29 +74,30 @@ app.use(
       exclude: [/node_modules[\\\/]/],
     },
   })
-)
+);
 
-app.use(hotMiddleware(compile))
+app.use(hotMiddleware(compile));
 
-app.use( //重定向到首页，
+app.use(
+  //重定向到首页，
   async (ctx, next) => {
-    const __indexHtml = indexHtml(ctx.url)
+    const __indexHtml = indexHtml(ctx.url);
     const filename = path.join(compile.outputPath, __indexHtml);
-    const htmlFile = await (new Promise(function(resolve, reject){
-        compile.outputFileSystem.readFile(filename, (err, result) => {
-          if (err){
-            reject(err);
-          }else{
-            resolve(result);
-          }
-        });
-     }))
-     ctx.type = 'html';
-     ctx.body = htmlFile;
-     await next()
+    const htmlFile = await new Promise(function(resolve, reject) {
+      compile.outputFileSystem.readFile(filename, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    ctx.type = "html";
+    ctx.body = htmlFile;
+    await next();
   }
-)
+);
 
 app.listen(3000, () => {
-    console.log('Example app listening om port 3000!\n');
+  console.log("Example app listening om port 3000!\n");
 });
--- bin/index.js
+++ bin/index.js
@@ -1,54 +1,51 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
-const packages = require('../package.json')
+const commander = require("commander");
+const packages = require("../package.json");
+
+commander.version(packages.version);
 
 commander
-  .version(packages.version)
-
-commander
-  .command('init')
-  .description('创建项目')
+  .command("init")
+  .description("创建项目")
   .allowUnknownOption()
   .action(async (options) => {
-    await require('./commands/init')(options)
-})
+    await require("./commands/init")(options);
+  });
 
 commander
-  .command('pack')
-  .description('打包💼')
-  .option('-p --prod', '打包线上版本')
-  .option('-m --min', '压缩优化')
-  .option('-n --node', '打包node server工程')
+  .command("pack")
+  .description("打包💼")
+  .option("-p --prod", "打包线上版本")
+  .option("-m --min", "压缩优化")
+  .option("-n --node", "打包node server工程")
   .allowUnknownOption()
   .action(async (options) => {
-    await require('./commands/pack')(options)
-})
+    await require("./commands/pack")(options);
+  });
 
 commander
-.command('server')
-.description('本地开发服务🐆')
-.allowUnknownOption()
-.action(async (options) => { 
-  try {
-    await require('./commands/server')(options)
-  }catch(err){
-    console.log('server--',err)
-  }
-    
-})
-
-commander
-  .command('dll')
-  .description('打包第三方模块🐆')
+  .command("server")
+  .description("本地开发服务🐆")
   .allowUnknownOption()
   .action(async (options) => {
     try {
-      await require('./commands/dll')(options)
-    }catch(err){
-      console.log('dllAction--',err)
+      await require("./commands/server")(options);
+    } catch (err) {
+      console.log("启动server失败 \r\n", err);
     }
-     
-  })
+  });
 
-commander.parse(process.argv)
+commander
+  .command("dll")
+  .description("打包第三方模块🐆")
+  .allowUnknownOption()
+  .action(async (options) => {
+    try {
+      await require("./commands/dll")(options);
+    } catch (err) {
+      console.log("dll打包失败\r\n", err);
+    }
+  });
+
+commander.parse(process.argv);
